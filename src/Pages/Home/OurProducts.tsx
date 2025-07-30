7773ca6d289a59cf17e8d8f65ddab47cef7115cc
najmul905 <nh4808563@gmail.com>
2025-07-30 09:44:26 +0600

    fixed some error and make a HomePeoductsData

--- src/Pages/Home/OurProducts.tsx
+++ src/Pages/Home/OurProducts.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
 
-const OurProducts = () => {
+
+import { Poppins } from 'next/font/google';
+import React from 'react';
+import ProductsCard from './ProductsCard';
+import path from 'path';
+import { promises as fs } from 'fs';
+
+const poppins_thin = Poppins({
+   subsets:["latin"],
+  weight: ['200']
+
+})
+interface Data{
+    _id:number,
+    Name:string,
+    Image:string,
+    Price:number,
+    Offer:number|null
+}
+const OurProducts =async () => {
+     const filePath = path.join(process.cwd(), 'public', 'HomeProductsData.json');
+      const fileContent = await fs.readFile(filePath, 'utf-8');
+      const data: Data[] = JSON.parse(fileContent);
+      console.log(data)
     return (
-        <div>
-            Our products
+        <div className='mt-32'>
+          <div className=' text-center'>
+    <h3 className={`text-[18px] text-[#82ae46] ${poppins_thin.className}`}>Featured Products</h3>
+        <h1 className='text-[35px] font-extrabold py-6'>Our Products</h1>
+        <p className='text-[12px] text-slate-500'>We are always try to provide best Vegetable and fruits for our all customer.</p>
+          </div>
+          <div className='mt-14'>
+        <ProductsCard data={data}></ProductsCard>
+          </div>
         </div>
     );
 };
---
+++ src/Pages/Home/ProductsCard.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import Image from 'next/image';
+import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useState } from 'react';
+interface Data{
+    _id:number,
+    Name:string,
+    Image:string,
+    Price:number,
+    Offer:number|null
+}
+
+
+const ProductsCard = ({ data }: { data: Data[] }) => {
+    const [view,setView]=useState<number | null>(null)
+    console.log(view) 
+    
+    return (
+        <div>
+            <div className='grid grid-cols-4 gap-5 mx-32'>
+                {
+                    data?.map(item=>(
+                        <div key={item._id}
+                        onMouseEnter={() => setView(item._id)}
+                        onMouseLeave={() => setView(null)}
+                        className={`border overflow-hidden h-[350px] p-12 border-slate-200 `}>
+                            <div className='h-[200px] w-full relative overflow-visible mx-auto'>
+                                <Image src={`${item.Image}`}  alt={item.Name} height={300} width={600} className='hover:scale-130 transition-transform duration-300 ease-in-out' ></Image>
+                            </div>
+                        <div className='text-center'>
+                            <h1 className='font-semibold text-[16px]'>{item.Name}</h1>
+                            <h3 className={ `text-[15px] py-3 font-bold text-slate-400 ${view===item._id&&"hidden"}`}>${item.Price}</h3>
+                            <div className={`py-3 ${view === item._id ? 'block' : 'hidden'}`}>
+                                <div className=' flex gap-3 text-center '>
+                                <button className='cursor-pointer border border-[#82ae46] hover:bg-transparent hover:text-[#82ae46] bg-[#82ae46] p-2 rounded-full '><MenuIcon fontSize='small'></MenuIcon></button>
+                                <button className=' cursor-pointer border border-[#82ae46] hover:bg-transparent hover:text-[#82ae46] bg-[#82ae46] p-2 rounded-full '><ShoppingCartIcon fontSize='small'></ShoppingCartIcon></button>
+                                <button className='cursor-pointer border border-[#82ae46] hover:bg-transparent hover:text-[#82ae46] bg-[#82ae46] p-2 rounded-full '><FavoriteBorderIcon fontSize='small'></FavoriteBorderIcon></button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ProductsCard;
