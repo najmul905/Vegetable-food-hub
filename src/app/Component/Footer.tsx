8df15152c9e08a11e74366ba74117b10e9674563
najmul905 <nh4808563@gmail.com>
2025-08-06 23:50:08 +0600

    setup Redux tolkit

--- src/app/Component/Footer.tsx
+++ src/app/Component/Footer.tsx
@@ -16,15 +16,15 @@ const Footer = () => {
                 <div className='h-[300px] relative '> <Image src={"https://i.ibb.co/hRwXjcvg/footer-image-1.jpg"} fill alt='Footer image' className='object-cover'></Image> </div>
                 <div className='absolute top-0 h-[300px] transparent-layer w-full footer_gradient opacity-70 flex items-center justify-center'>
                 <div className='flex items-center justify-center text-white'>
-                    <div className='w-1/2 h-full ps-12 '>
+                    <div className='w-1/2 h-full flex flex-col gap-4 ps-12 '>
                     <h1 className='text-[14px]'><LocationPinIcon fontSize='small'></LocationPinIcon> Sunderganj, Gaibandha,Dhaka,Bangladesh</h1>
                     <h1 className='text-[14px]'><MailIcon fontSize='small'></MailIcon> <span className='text-green-900 font-bold'>muhammadnajmulislam19@gmail.com</span></h1>
-                    <h1 className='text-[14px]'><PhoneEnabledIcon fontSize='small'></PhoneEnabledIcon> +8801723165412</h1>
+                    <h1 className='text-[14px]'><PhoneEnabledIcon fontSize='small'></PhoneEnabledIcon>+8801723165412</h1>
                     </div>
                     <div className='w-1/2 h-full'>
                     <h1 className='text-center text-2xl font-semibold'>About the company</h1>
-                    <p className='pe-12'>This website is for sell fresh organic vegetables and fruits. We are Always try to provide our best products to our customer for there healthy health . We are always try to do our best and you all customer keep use in your prayer</p>
-                    <div><FacebookIcon></FacebookIcon> <TwitterIcon></TwitterIcon> <InstagramIcon></InstagramIcon> <LinkedInIcon></LinkedInIcon> </div>
+                    <p className='pe-12 py-5 text-center text-[13px]'>This website is for sell fresh organic vegetables and fruits. We are Always try to provide our best products to our customer for there healthy health . We are always try to do our best and you all customer keep use in your prayer</p>
+                    <div className='w-full flex items-center justify-center gap-5'><a href="https://www.facebook.com/profile.php?id=100082173193515"><FacebookIcon ></FacebookIcon></a> <a href="https://x.com/Nazmul905"><TwitterIcon></TwitterIcon></a> <a href=" https://www.instagram.com/muhammadnajmul905/"><InstagramIcon></InstagramIcon></a> <a href="https://www.linkedin.com/in/najmul-islam-dev/ "><LinkedInIcon></LinkedInIcon> </a></div>
                     </div>
                 </div>
                 </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono,Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Component/Navbar/Navbar";
+import { Provider } from "react-redux";
+import { store } from "@/Redux/store";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.className} text-[16px] antialiased`}
       >
+        <Provider store={store}></Provider>
         <Navbar></Navbar>
         {children}
       </body>
